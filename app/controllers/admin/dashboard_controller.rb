71ada603fe53aea3e95e67cf81d7b2cd72f569b7
Youssef Nafaa <nafaayoussef@gmail.com>
2022-01-06 12:25:01 -0500

    count products and categories from database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class Admin::DashboardController < ApplicationController
-
+  # Add basic authentication
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
-  
-  def show
+  # Add number of products and number of categories
+  def show # Show them from database
     @product_count = Product.count
     @category_count = Category.count
   end
